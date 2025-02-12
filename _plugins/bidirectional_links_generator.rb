9d3dd8bee2c0e0a20430aad407f002b4d1018940
Felix Imobersteg <mail@feliximobersteg.ch>
2025-02-12 23:02:27 +0100

    Remove unused notes graph

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 class BidirectionalLinksGenerator < Jekyll::Generator
   def generate(site)
-    graph_nodes = []
-    graph_edges = []
-
     all_notes = site.collections['notes'].docs
     all_pages = site.pages
 
@@ -80,29 +77,9 @@ class BidirectionalLinksGenerator < Jekyll::Generator
         e.url != current_note.url && e.content.include?(current_note.url)
       end
 
-      # Nodes: Graph
-      graph_nodes << {
-        id: note_id_from_note(current_note),
-        path: "#{site.baseurl}#{current_note.url}#{link_extension}",
-        label: current_note.data['title'],
-      } unless current_note.path.include?('_notes/index.html')
-
       # Edges: Jekyll
       current_note.data['backlinks'] = notes_linking_to_current_note
-
-      # Edges: Graph
-      notes_linking_to_current_note.each do |n|
-        graph_edges << {
-          source: note_id_from_note(n),
-          target: note_id_from_note(current_note),
-        }
-      end
     end
-
-    File.write('_includes/notes_graph.json', JSON.dump({
-      edges: graph_edges,
-      nodes: graph_nodes,
-    }))
   end
 
   def note_id_from_note(note)
